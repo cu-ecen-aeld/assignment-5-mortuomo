07a27f14724144a0541e8d9fd1231c26bb9b289c
Stefano Archini <stefanoarchini00@gmail.com>
2025-10-04 14:20:34 +0200

    Assignment 9 - Update commit hash for aesd-assignment repo

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 6c27651638ab37991ffebbcd7e3dfa20561af1cf
+AESD_ASSIGNMENTS_VERSION = 7cffacb3eb2fe1bb291ed694f75408cf985734c7
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
